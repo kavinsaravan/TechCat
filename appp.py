e7ac1d950c9cb6259fdab1b61403a71477f20d52
Kavin <kavinsaravan@gmail.com>
2024-12-30 00:39:15 -0800

    initial commit

---
+++ appp.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+import json
+
+client = OpenAI()
+
+f = open("coroutines.json", "a")
+
+topic = "Kotlin coroutines"
+count = 50
+word = "algorithms"
+
+messages = [
+    {"role": "system", "content": "You are an assistant that can answer either YES or NO based on the given prompt."},
+    {"role": "system", "content": "If the user types a word that has any relation to technology, answer with YES"},
+    {"role": "system", "content": "As an example, if the user types 'Compiler', 'Python', or 'Java', answer with YES"},
+    {"role": "system", "content": "If the user types a word that does not have any relation to technology, answer with NO"},
+    {"role": "system", "content": "As an example, if the user types 'Water', 'Pizza', or 'Exercise', answer with NO"},
+    {"role": "user", "content": f"Tell me whether the word I type is related to technology: {word}"}
+]
+
+result = []
+
+
+print(f"Run ...")
+response = client.chat.completions.create(
+    model = "gpt-4o-mini",
+    messages = messages
+)
+
+response_dict = response.to_dict()
+print(response_dict)
+
+# Access the message content
+answer = response_dict['choices'][0]['message']['content']
+print("answer", answer)
+
+output = json.dumps(result)
+f.write(output)
+f.close()
